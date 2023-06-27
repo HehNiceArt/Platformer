6048dc3934132c0e054ef29ca3e2cebaa2521366
Andrei Quirante <122663831+ADBQ@users.noreply.github.com>
2023-06-27 15:05:18 +0800

    Added traps and animation
    Added traps and animation
    Added collision
    Added coin collection
    Added animation trigger upon collision

--- Assets/Scrpits/PlayerBehavior.cs
+++ Assets/Scrpits/PlayerBehavior.cs
@@ -17,6 +17,8 @@ public class PlayerBehavior : MonoBehaviour
     //Access our Animator
     public Animator anim;
 
+    public int CoinCount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +30,7 @@ public class PlayerBehavior : MonoBehaviour
     void Update()
     {
 
+        /*
         //if W key is pressed, trigger if else
         if (Input.GetKeyDown(KeyCode.W) || (Input.GetKeyDown(KeyCode.UpArrow)))
         {
@@ -44,12 +47,12 @@ public class PlayerBehavior : MonoBehaviour
                 Debug.Log("Press Up Arrow!");
             }
         }
-
-       /* if (Input.GetKeyUp(KeyCode.W) || (Input.GetKeyUp(KeyCode.UpArrow)))
+        
+        if (Input.GetKeyUp(KeyCode.W) || (Input.GetKeyUp(KeyCode.UpArrow)))
         {
             anim.SetTrigger("Knight_Backward_Pause");
             Debug.Log("Backward Animation Pause");
-        }*/
+        }
 
         if (Input.GetKeyDown(KeyCode.S) || (Input.GetKeyDown(KeyCode.DownArrow)))
         {
@@ -65,11 +68,11 @@ public class PlayerBehavior : MonoBehaviour
             }
         }
         
-        /*if (Input.GetKeyUp(KeyCode.S) || (Input.GetKeyUp(KeyCode.DownArrow))) 
+        if (Input.GetKeyUp(KeyCode.S) || (Input.GetKeyUp(KeyCode.DownArrow))) 
         {
             anim.SetTrigger("Knight_Forward_Pause");
             Debug.Log("Forward Animation Pause");
-        }*/
+        }
 
         if (Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.LeftArrow))) 
         {
@@ -85,11 +88,11 @@ public class PlayerBehavior : MonoBehaviour
             }
         }
 
-        /*if (Input.GetKeyUp(KeyCode.A) || (Input.GetKeyUp(KeyCode.LeftArrow)))
+        if (Input.GetKeyUp(KeyCode.A) || (Input.GetKeyUp(KeyCode.LeftArrow)))
         {
             anim.SetTrigger("Knight_Left_Pause");
             Debug.Log("Left Animation Pause");
-        }*/
+        }
 
         if (Input.GetKeyDown(KeyCode.D) || (Input.GetKeyDown(KeyCode.RightArrow)))
         {
@@ -105,17 +108,25 @@ public class PlayerBehavior : MonoBehaviour
             }
         }
 
-        /*if (Input.GetKeyUp(KeyCode.D) || (Input.GetKeyUp(KeyCode.RightArrow)))
+        if (Input.GetKeyUp(KeyCode.D) || (Input.GetKeyUp(KeyCode.RightArrow)))
         {
             anim.SetTrigger("Knight_Right_Pause");
             Debug.Log("Right Animation Pause");
-        }*/
+        }
+        
 
         if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
         {
             anim.enabled = false;
             Debug.Log("anim.enabled = false");
         }
+        */
+
+        anim.SetFloat("Horizontal", movementInput.x);
+        //Debug.Log("MovementInput X");
+        anim.SetFloat("Vertical", movementInput.y);
+        //Debug.Log("MovementInput Y");
+        anim.SetFloat("Speed", movementInput.sqrMagnitude);
     }
 
     //Fixed update for physics calculations
@@ -132,5 +143,20 @@ public class PlayerBehavior : MonoBehaviour
         //if press WASD =  to Vector 2 Values
         movementInput = inputValue.Get<Vector2>();
         
+    }
+
+    //On trigger - bool
+    //(Enter) - if player enters the collision area
+    //(Exit) - if player is not detected within collision area
+    //(Stay) - if player is within collision area
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin")) 
+        {
+            Destroy(collision.gameObject);
+            CoinCount++;
+            Debug.Log("+1 coin");
+        }
     }
 }
